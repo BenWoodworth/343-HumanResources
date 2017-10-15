f411eb13502d2219fc790474769479823ba736ba
beltrancaliz <bjc8008@rit.edu>
2017-10-15 14:39:03 -0400

    added employee db

---
+++ employeedb.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE  IF NOT EXISTS `hr_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hr_db`;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  `phoneNumber` varchar(10) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FOREIGN` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
