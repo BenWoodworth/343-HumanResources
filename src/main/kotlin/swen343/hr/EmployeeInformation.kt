5fd11f607e7a579e1ff9dae7844ceccd171a4b3d
jahn621 <jka1284@rit.edu>
2017-10-15 20:40:50 -0400

    Created Data class EmployeeInformation.kt
    Created new properties for an Employee.

---
+++ src/main/kotlin/swen343/hr/EmployeeInformation.kt
@@ -0,0 +1,18 @@
+package swen343.hr
+
+import java.util.*
+
+data class EmployeeInformation (
+        val firstName: String,
+        val lastName: String,
+        val birthday: Date,
+        val phoneNumber: String,
+        val altPhoneNumber: String,
+        val email: String,
+        val employeeID: String,
+        val username: String,
+        val password: String,
+        val jobTitle: String,
+        val supervisor: String,
+        val payroll: String
+)
