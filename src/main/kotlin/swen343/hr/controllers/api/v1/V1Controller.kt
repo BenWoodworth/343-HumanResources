d3698ec87bea100a656d9856eb447e045f49014d
BenWoodworth <ben@benwoodworth.net>
2017-10-16 22:27:57 -0400

    Commented out TODO

---
+++ src/main/kotlin/swen343/hr/controllers/api/v1/V1Controller.kt
@@ -0,0 +1,13 @@
+package swen343.hr.controllers.api.v1
+
+import spark.RouteGroup
+
+/**
+ * Created by ben on 10/16/17.
+ */
+class V1Controller : RouteGroup {
+
+    override fun addRoutes() {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
