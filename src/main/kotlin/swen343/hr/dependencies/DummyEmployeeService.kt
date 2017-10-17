c7d1d1d9d28f23e86b1c87cca60fe1c3733784c2
BenWoodworth <ben@benwoodworth.net>
2017-10-16 23:57:51 -0400

    Added dummy objects to DummyEmployeeService

--- src/main/kotlin/swen343/hr/dependencies/DummyEmployeeService.kt
+++ src/main/kotlin/swen343/hr/dependencies/DummyEmployeeService.kt
@@ -9,6 +9,32 @@ class DummyEmployeeService : EmployeeService {
 
     private val employees = mutableListOf<Employee>()
 
+    init {
+        employees.add(Employee(
+                id = 0,
+                firstName = "John",
+                lastName = "Doe",
+                title = "Project Manager",
+                department = "IT",
+                salary = 100_000,
+                phoneNumber = "5555555555",
+                email = "john_doe@example.com",
+                address = "123 Fake Ave."
+        ))
+
+        employees.add(Employee(
+                id = 1,
+                firstName = "Jane",
+                lastName = "Doe",
+                title = "Sales Rep",
+                department = "HR",
+                salary = 80_000,
+                phoneNumber = "5555555554",
+                email = "jane_doe@example.com",
+                address = "123 Fake Ave."
+        ))
+    }
+
     override fun addEmployee(employee: Employee) {
         deleteEmployee(employee)
         employees.add(employee)
