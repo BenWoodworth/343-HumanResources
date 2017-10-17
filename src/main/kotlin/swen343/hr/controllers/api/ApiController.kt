daa888ed52f89ff81a206cac7646cae18795c649
BenWoodworth <ben@benwoodworth.net>
2017-10-16 23:36:53 -0400

    Work on ApiController

--- src/main/kotlin/swen343/hr/controllers/api/ApiController.kt
+++ src/main/kotlin/swen343/hr/controllers/api/ApiController.kt
@@ -3,6 +3,7 @@ package swen343.hr.controllers.api
 import spark.RouteGroup
 import spark.Spark.path
 import spark.kotlin.get
+import swen343.hr.Employee
 import swen343.hr.controllers.api.v1.V1Controller
 
 /**
@@ -13,11 +14,16 @@ class ApiController : RouteGroup {
     override fun addRoutes() {
         path("/v1", V1Controller())
 
-        get("employees/:id/salary") {
-            val id = request.params("id")
+        get("employees/:id") {
+            val employee: Employee = TODO()
 
             """
-
+            {
+                "employee": {
+                    "id": ${employee.id},
+                    "salary": ${employee.salary}
+                }
+            }
             """.trimIndent()
         }
     }
