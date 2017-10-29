ab5de2885aff250771adade9f1292384396f5483
BenWoodworth <ben@benwoodworth.net>
2017-10-29 17:33:22 -0400

    Create avatar api

--- src/main/kotlin/swen343/hr/controllers/api/v1/ControllerV1.kt
+++ src/main/kotlin/swen343/hr/controllers/api/v1/ControllerV1.kt
@@ -1,20 +1,18 @@
 package swen343.hr.controllers.api.v1
 
 import spark.RouteGroup
-import spark.kotlin.get
+import spark.Spark
+import swen343.hr.controllers.api.v1.employees.ControllerEmployeesApi
 import swen343.hr.dependencies.EmployeeService
 
 /**
  * Created by ben on 10/16/17.
  */
-class V1Controller(
+class ControllerV1(
         private val employeeService: EmployeeService
 ) : RouteGroup {
 
     override fun addRoutes() {
-
-        get("/employees") {
-
-        }
+        Spark.path("/employees", ControllerEmployeesApi(employeeService))
     }
 }
