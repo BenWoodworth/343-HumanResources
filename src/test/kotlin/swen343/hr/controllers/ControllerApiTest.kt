463d328b141ffc8ad76fb82dc5ed25189302ebdb
BenWoodworth <ben@benwoodworth.net>
2017-11-03 17:00:28 -0400

    Did a thing for Jack

--- src/test/kotlin/swen343/hr/controllers/ControllerApiTest.kt
+++ src/test/kotlin/swen343/hr/controllers/ControllerApiTest.kt
@@ -3,7 +3,9 @@ package swen343.hr.controllers
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
+
 import swen343.hr.controllers.api.ControllerApi
 import swen343.hr.dependencies.EmployeeService
 
@@ -11,6 +13,7 @@ class ControllerApiTest: Spek({
 
     describe("Controller for the API."){
         val employeeService = mock(EmployeeService::class.java)
+        `when`(employeeService.getEmployee("dad")).thenReturn(null)
 
         val api = ControllerApi()
 
