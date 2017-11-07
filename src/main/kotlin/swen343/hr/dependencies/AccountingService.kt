d0c5aeaad35357328b6cea9d2184a0815dee85dc
beltrancaliz <bjc8008@rit.edu>
2017-11-07 00:00:47 -0500

    init accounting service

---
+++ src/main/kotlin/swen343/hr/dependencies/AccountingService.kt
@@ -0,0 +1,8 @@
+package swen343.hr.dependencies
+
+import com.google.inject.ImplementedBy
+
+@ImplementedBy(AccountingServiceHttp::class)
+interface AccountingService {
+
+}
---
+++ src/main/kotlin/swen343/hr/dependencies/AccountingServiceHttp.kt
@@ -0,0 +1,12 @@
+package swen343.hr.dependencies
+
+import com.google.inject.ImplementedBy
+
+
+class AccountingServiceHttp {
+    //TODO Waiting for Accounting implementation
+    fun getBudget(){
+
+    }
+
+}
