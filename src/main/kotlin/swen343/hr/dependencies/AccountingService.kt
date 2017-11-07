933d54efaf64673673c15e30525a7606cffd9831
BenWoodworth <ben@benwoodworth.net>
2017-11-07 00:48:59 -0500

    Modify AccountingService

--- src/main/kotlin/swen343/hr/dependencies/AccountingService.kt
+++ src/main/kotlin/swen343/hr/dependencies/AccountingService.kt
@@ -1,9 +1,21 @@
 package swen343.hr.dependencies
 
 import com.google.inject.ImplementedBy
+import swen343.hr.models.Employee
 
 @ImplementedBy(AccountingServiceHttp::class)
 interface AccountingService {
 
+    /**
+     * Get this department's budget.
+     */
     fun getBudget(): Int
+
+    /**
+     * Request for an employee's salary to be taken out of
+     * the department's budget.
+     *
+     * @return `true` iff the request was successful
+     */
+    fun requestPayroll(employee: Employee): Boolean
 }
--- src/main/kotlin/swen343/hr/dependencies/AccountingServiceHttp.kt
+++ src/main/kotlin/swen343/hr/dependencies/AccountingServiceHttp.kt
@@ -1,9 +1,14 @@
 package swen343.hr.dependencies
 
 import com.google.inject.Singleton
+import swen343.hr.models.Employee
 
 @Singleton
 class AccountingServiceHttp : AccountingService {
+
+    override fun requestPayroll(employee: Employee): Boolean {
+        TODO("Waiting for Accounting implementation")
+    }
 
     override fun getBudget(): Int {
         TODO("Waiting for Accounting implementation")
