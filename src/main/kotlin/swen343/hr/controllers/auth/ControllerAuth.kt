e23dacaadb8f1c8a98baa08707b08bc08db4b451
BenWoodworth <ben@benwoodworth.net>
2017-12-04 22:05:16 -0500

    Revert ControllerAuth changes

--- src/main/kotlin/swen343/hr/controllers/auth/ControllerAuth.kt
+++ src/main/kotlin/swen343/hr/controllers/auth/ControllerAuth.kt
@@ -2,15 +2,11 @@ package swen343.hr.controllers.auth
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import spark.Response
 import spark.RouteGroup
-import spark.kotlin.RouteHandler
 import spark.kotlin.get
 import spark.kotlin.post
-import swen343.hr.controllers.RequestHandler
 import swen343.hr.dependencies.TemplateLoader
 import swen343.hr.dependencies.UserService
-import swen343.hr.models.User
 import swen343.hr.util.RouteUtil
 import swen343.hr.viewmodels.FormLogin
 
@@ -19,78 +15,44 @@ import swen343.hr.viewmodels.FormLogin
  */
 @Singleton
 class ControllerAuth @Inject constructor(
-        private val routeUtil: RouteUtil,
-
-        private val getLogin: GetLogin,
-        private val postLogin: PostLogin
+        private val userService: UserService,
+        private val templateLoader: TemplateLoader,
+        private val formLoginFactory: FormLogin.Factory,
+        private val routeUtil: RouteUtil
 ) : RouteGroup {
 
     override fun addRoutes() {
 
         get("login/") {
-            getLogin(this)
+
+            templateLoader.loadTemplate(
+                    "/auth/login.ftl",
+                    formLoginFactory.getForm(routeUtil.user(this))
+            )
         }
 
         post("login/") {
-            postLogin(this)
+            val form = formLoginFactory.getForm(
+                    sessionUser = routeUtil.user(this),
+                    username = request.queryParams("username"),
+                    password = request.queryParams("password")
+            )
+
+            if (!form.validate()) {
+                templateLoader.loadTemplate(
+                        "/auth/login.ftl",
+                        form
+                )
+            } else {
+
+                routeUtil.user(this, userService.getUser(form.fields.username))
+                response.redirect("/silos/")
+            }
         }
 
         get("sign-out/") {
             routeUtil.user(this, null)
             response.redirect("/")
-        }
-    }
-
-    class GetLogin @Inject constructor(
-            private val formLoginFactory: FormLogin.Factory
-    ) : RequestHandler.Basic() {
-
-        override fun handleRequest(routeHandler: RouteHandler) {
-            templateLoader.loadTemplate(
-                    "/auth/login.ftl",
-                    formLoginFactory.getForm(routeUtil.user(routeHandler))
-            )
-        }
-    }
-
-    class PostLogin @Inject constructor(
-            private val formLoginFactory: FormLogin.Factory,
-            private val userService: UserService
-    ) : RequestHandler<FormLogin>() {
-
-        class Data(
-                val username: String,
-                val password: String
-        )
-
-        override val dataMapper: RouteHandler.() -> FormLogin = {
-            formLoginFactory.getForm(
-                    sessionUser = routeUtil.user(this),
-                    username = queryParams("username"),
-                    password = queryParams("password")
-            )
-        }
-
-        override fun handleRequest(data: FormLogin, routeHandler: RouteHandler) {
-            if (!data.validate()) {
-                templateLoader.loadTemplate(
-                        "/auth/login.ftl",
-                        data
-                )
-            } else {
-                routeUtil.user(routeHandler, userService.getUser(data.fields.username))
-                routeHandler.response.redirect("/silos/")
-            }
-        }
-    }
-
-    class GetSignOut @Inject constructor() : RequestHandler.Basic() {
-
-        override fun handleRequest(routeHandler: RouteHandler) {
-            templateLoader.loadTemplate(
-                    "/auth/login.ftl",
-                    formLoginFactory.getForm(routeUtil.user(routeHandler))
-            )
         }
     }
 }
