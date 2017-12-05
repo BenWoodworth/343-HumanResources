f5c7d040f4e9d1d4cc585402ed62cb849f49e60a
jahn621 <jka1284@rit.edu>
2017-12-04 20:05:29 -0500

    Added more skeleton test code

---
+++ src/test/kotlin/swen343/hr/testSalaryAPI.kt
@@ -0,0 +1,13 @@
+package swen343.hr
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class testSalaryAPI:Spek({
+    describe("Test the value received for the salary API"){
+        it("User should be able to get the salary"){}
+        it("User should not be able to get the salary"){}
+    }
+
+})
