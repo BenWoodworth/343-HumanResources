3821b43f87b5c5f67189be9b0c45188e15a1f71f
beltrancaliz <bjc8008@rit.edu>
2017-12-04 23:27:25 -0500

    added tests

--- src/test/kotlin/swen343/hr/controllers/testControllerEmployees.kt
+++ src/test/kotlin/swen343/hr/controllers/testControllerEmployees.kt
@@ -3,16 +3,76 @@ package swen343.hr.controllers
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import spark.Spark
+import javax.xml.ws.http.HTTPException
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class testControllerEmployees : Spek({
     describe("Tests for addRoutes function"){
-        it("User should be able to view employees by getting the 'employee/' path"){}
-        it("User should be able to add employees by getting the employee/add path"){}
-        it("User should be able to add employees by posting the employee/add path"){}
-        it("User should be able to edit employees by getting the employee/edit/:username path"){}
-        it("User should be able to edit employees by posting the employee/edit/:username path"){}
-        it("User should be able to delete employees by getting the employee/delete/:username path"){}
-        it("User should be bale to view employees by getting the employee/view/:username path"){}
+        it("User should be able to view employees by getting the 'employee/' path"){
+            try {
+                Spark.connect("/employees/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be able to add employees by getting the employee/add path"){
+            try {
+                Spark.connect("/employees/add/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be able to add employees by posting the employee/add path"){
+            try {
+                Spark.connect("/employees/add/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be able to edit employees by getting the employee/edit/:username path"){
+            try {
+                Spark.connect("/employees/edit/jahn/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be able to edit employees by posting the employee/edit/:username path"){
+            try {
+                Spark.connect("/employees/edit/jahn/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be able to delete employees by getting the employee/delete/:username path"){
+            try {
+                Spark.connect("/employees/delete/jahn/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("User should be bale to view employees by getting the employee/view/:username path"){
+            try {
+                Spark.connect("/employees/view/jahn/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
 
     }
 })
--- src/test/kotlin/swen343/hr/controllers/testControllerIndex.kt
+++ src/test/kotlin/swen343/hr/controllers/testControllerIndex.kt
@@ -3,15 +3,67 @@ package swen343.hr.controllers
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import spark.Spark
+import javax.xml.ws.http.HTTPException
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class testControllerIndex: Spek({
     describe("Tests for addRoutes in the Index."){
-        it("Users access controllerApi by using the path /api"){}
-        it("Users access controllerEmployees by using the path /employees"){}
-        it("Users access controllerUsers by using the path /users"){}
-        it("Users access controllerAuth by using the path /auth"){}
-        it("Users are redirected to the log in when not logged in when getting the '/' path"){}
-        it("Users are directed to the silos page when signed in when getting the /silos path"){}
+        it("Users access controllerApi by using the path /api"){
+            try {
+                Spark.connect("/api/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("Users access controllerEmployees by using the path /employees"){
+            try {
+                Spark.connect("/employees/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("Users access controllerUsers by using the path /users"){
+            try {
+                Spark.connect("/users/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("Users access controllerAuth by using the path /auth"){
+            try {
+                Spark.connect("/auth/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("Users are redirected to the log in when not logged in when getting the '/' path"){
+            try {
+                Spark.connect("/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
+        it("Users are directed to the silos page when signed in when getting the /silos path"){
+            try {
+                Spark.connect("/silos/", "GET") { _, response ->
+                    assertEquals(200, response.status())
+                }
+            } catch (exception: HTTPException) {
+                fail("Should not throw exception")
+            }
+        }
     }
 
 })
